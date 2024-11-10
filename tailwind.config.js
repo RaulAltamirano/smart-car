4d516100cdf7ad23bbe727b9f0de4920f6469554
Pacman <rulomania2011@hotmail.com>
2024-11-09 21:18:53 -0600

    Create(tailwind) Create config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#1D4ED8",
+        success: "#4ADE80",
+        warning: "#FBBF24",
+        error: "#F87171",
+      },
+    },
   },
+
   plugins: [],
 }
-
